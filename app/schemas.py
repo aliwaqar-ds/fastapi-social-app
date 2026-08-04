4a6546e8177fb02bc9b8e7d37758681c422216cf
aliwaqar0328-design <ali.waqar0328@gmail.com>
2026-08-04 16:57:22 +0500

    Implement post relationships, user authentication, and route protection

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,EmailStr
 from datetime import datetime
 
 
@@ -17,6 +17,24 @@ class Post(PostBase):
 
     class config:
         orm_mode = True
+
+class UserCreate(BaseModel):
+    email : EmailStr
+    password : str
+
+class UserOut(BaseModel):
+    id : int
+    email : EmailStr
+    created_at : datetime
+
+    class config:
+        orm_mode = True
+
+class UserLogin(BaseModel):
+    email : EmailStr
+    password : str
+     
+
 
 
 
